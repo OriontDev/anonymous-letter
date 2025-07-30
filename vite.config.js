875005ed28d27cb231807edb83d733f29088e87f
OriontDev <madeoriontfedora@gmail.com>
2025-07-30 14:13:20 +0800

    set up for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/anonymous-letter/', // <-- use your repo name here
   plugins: [react()],
-})
+});
